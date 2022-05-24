4e1c7902bf6a223daf7088772a6a3f1d46b64733
Rob Anderson <robandpdx@github.com>
2022-05-23 19:59:45 -0600

    try this

--- .github/scripts/initialize-request.js
+++ .github/scripts/initialize-request.js
@@ -4,5 +4,6 @@ const fs = require("fs");
 let octokit;
 
 module.exports = async ({github, context, payload}) => {
-    console.log(`Found the following payload: ${JSON.stringify(payload)}`);
+    console.log(`initialize-request.js`);
+    //console.log(`Found the following payload: ${JSON.stringify(payload)}`);
 }
