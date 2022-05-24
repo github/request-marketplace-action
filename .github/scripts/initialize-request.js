93c542a5559777ee624b5375043467814bffa487
Rob Anderson <robandpdx@github.com>
2022-05-23 20:21:40 -0600

    try again

--- .github/scripts/initialize-request.js
+++ .github/scripts/initialize-request.js
@@ -4,6 +4,6 @@ const fs = require("fs");
 let octokit;
 
 module.exports = async ({github, context, payload}) => {
-    console.log(`initialize-request.js`);
+    //console.log(`initialize-request.js`);
     //console.log(`Found the following payload: ${JSON.stringify(payload)}`);
 }
