07a9db70cfdc7e40833c240e7ec1e19e84053d36
Rob Anderson <robandpdx@github.com>
2022-06-01 16:23:31 -0500

    cleanup and comments

--- .github/scripts/initialize-request.js
+++ .github/scripts/initialize-request.js
@@ -11,18 +11,18 @@ module.exports = async ({github, context, payload, options}) => {
     });
 
     let exitError = false;
-    //let repo;
     // check if the repo already exists
     let repo = await octokit.request(`GET /repos/${actionsApprovedOrg}/${payload.repo}_${payload.ref}`, {
         owner: actionsApprovedOrg,
         repo: `${payload.repo}_${payload.ref}`
     })
     .then(response => {
-        console.log(response);
+        // Repo exists, so we will fail the remainder of the workflow
         console.log(`Repo ${payload.repo}_${payload.ref} already exists`);
         exitError = true;
     })
     .catch(async error => {
+        // Repo does not exist, so we will create it
         console.log(error);
         console.log(`Creating repo ${payload.repo}_${payload.ref}`);
         let response = await octokit.request(`POST /orgs/${actionsApprovedOrg}/repos`, {
@@ -40,23 +40,4 @@ module.exports = async ({github, context, payload, options}) => {
     if (exitError) {
         throw new Error(`Repo ${actionsApprovedOrg}/${payload.repo}_${payload.ref} already exists`);
     }
-
-    // create new repo
-    // if (repo.status === 404) {
-    //     console.log(`Creating repo ${payload.repo}`);
-    //     let response = await octokit.request(`POST /orgs/${actionsApprovedOrg}/repos`, {
-    //         org: actionsApprovedOrg,
-    //         name: payload.repo,
-    //         description: `${payload.owner}/${payload.repo}@${payload.ref}`,
-    //         homepage: `https://github.com/${payload.owner}/${payload.repo}`,
-    //         'private': true,
-    //         has_issues: true,
-    //         has_projects: false,
-    //         has_wiki: false
-    //     });
-    // } else {
-    //     console.log(`Repo ${payload.repo} already exists`);
-    //     //throw new Error(`Repo ${payload.repo} already exists`);
-    // }
-    //console.log(JSON.stringify(response));
 }
